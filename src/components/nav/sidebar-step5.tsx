abad8c4a09c0f3c5d9c620c98520cfb6b4180152
Ross Perry <perryr16@gmail.com>
2021-04-14 15:35:02 -0600

    import ref hook

--- src/components/nav/sidebar-step5.tsx
+++ src/components/nav/sidebar-step5.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Link} from 'react-router-dom';
 import {stepRef} from '../../constants/index'
+import {ContextMenu} from '../../components/index'
+
 
 interface Props {
   handleStep:any
@@ -8,11 +10,13 @@ interface Props {
 
 export const SidebarStep5: React.FC<Props> = (props) => {
   const {handleStep} = props;
-  
+
   return (
     <div className='proj-details'>
-       <Link to={`/step5`} className='btn-edit-step' onClick={handleStep(5)}>EDIT</Link>
-       <p className='bold'>5: {stepRef[5]}</p>
+      <Link to={`/step5`} className='btn-edit-step' onClick={handleStep(5)}>EDIT</Link>
+      <p className='bold'>5: {stepRef[5]}</p>
+      <ContextMenu parentRef={containerRef} items={menuItems} />
+
     </div>
  );
 }
